34edc696e8f41783ba5ed6fc52873ba323eefe6b
Alexander Hildebrandt <alex@hilde.dev>
2025-11-22 23:00:18 +0100

    use zod for post backend

--- src/routes/write/+page.server.ts
+++ src/routes/write/+page.server.ts
@@ -1,26 +1,31 @@
 import type { Actions, PageServerLoad } from './$types'
 import { resolve } from '$app/paths'
-import { fail, redirect } from '@sveltejs/kit'
+import { error, redirect } from '@sveltejs/kit'
 import { createPosting } from '$lib/server/db'
 import { fetchFilenames, moveNewImages } from '$lib/server/filesystem.ts'
+import { z } from 'zod'
 
 export const load: PageServerLoad = async () => {
   return { filenames: fetchFilenames() }
 }
 
+const postSchema = z.object({
+  title: z.string().nonempty(),
+  description: z.string(),
+  content: z.string().nonempty()
+})
+
 export const actions: Actions = {
   default: async ({ request }) => {
     const formData = Object.fromEntries(await request.formData())
-    const content = formData?.content
-    if (!content) {
-      return fail(422)
+    const result = postSchema.safeParse(formData)
+    if (!result.success) {
+      error(422, result.error)
     }
 
-    const id = await createPosting(
-      formData?.title as string,
-      formData?.description as string,
-      content as string
-    )
+    const post = result.data
+
+    const id = await createPosting(post.title, post.description, post.content)
     moveNewImages(id)
 
     return redirect(302, resolve('/'))
