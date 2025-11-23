b4464d64132ba6b7d2aa356160b555190c976c1c
Alexander Hildebrandt <alex@hilde.dev>
2025-11-23 20:00:12 +0100

    add admin to schema

---
+++ drizzle/0004_even_master_mold.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `admin` (
+	`id` text PRIMARY KEY NOT NULL,
+	FOREIGN KEY (`id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,22 +2,31 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { sql } from 'drizzle-orm'
 
 export const user = sqliteTable('user', {
-  id: text('id').primaryKey(),
-  username: text('username').notNull().unique(),
+  id: text().primaryKey(),
+  username: text().notNull().unique(),
   passwordHash: text('password_hash').notNull()
 })
 
-export type Session = typeof session.$inferSelect
+export type User = typeof user.$inferSelect
+
+export const admin = sqliteTable('admin', {
+  id: text()
+    .notNull()
+    .references(() => user.id)
+    .primaryKey(),
+})
+
+export type Admin = typeof admin.$inferSelect
 
 export const session = sqliteTable('session', {
-  id: text('id').primaryKey(),
+  id: text().primaryKey(),
   userId: text('user_id')
     .notNull()
     .references(() => user.id),
   expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
 })
 
-export type User = typeof user.$inferSelect
+export type Session = typeof session.$inferSelect
 
 export const posting = sqliteTable('posting', {
   id: text().primaryKey(),
