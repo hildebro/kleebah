1b3de49993cda71e8bad6b75eba684805af45fd1
Alexander Hildebrandt <alex@hilde.dev>
2025-12-03 20:22:57 +0100

    feat: nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
   import '../app.css'
   import '$lib/styles/main.css'
   import favicon from '$lib/assets/favicon.svg'
+  import { resolve } from '$app/paths'
+  import * as m from '$lib/paraglide/messages'
 
   let { children } = $props()
 </script>
@@ -9,5 +11,15 @@
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
+
+<nav class="p-5 flex flex-col gap-3 sm:flex-row">
+  <a href={resolve('/')}>{m.home()}</a>
+  <a href={resolve('/posting/create')}>{m.create_posting()}</a>
+  <a href={resolve('/posting')}>{m.posting_edit()}</a>
+  <a href={resolve('/read')}>{m.read_postings()}</a>
+  <a href={resolve('/subscriber')}>{m.subscribers()}</a>
+  <a href={resolve('/feed.xml')}>{m.rss_feed()}</a>
+  <a href={resolve('/settings')}>{m.settings()}</a>
+</nav>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,5 @@
 <script lang="ts">
-  import { resolve } from '$app/paths'
   import * as m from '$lib/paraglide/messages'
 </script>
 
 <h1>{m.headline()}</h1>
-<a href={resolve('/posting/create')}>{m.create_posting()}</a>
-<br />
-<a href={resolve('/posting')}>{m.posting_edit()}</a>
-<br />
-<a href={resolve('/read')}>{m.read_postings()}</a>
-<br />
-<br />
-<a href={resolve('/subscriber')}>{m.subscribers()}</a>
-<br />
-<br />
-<a href={resolve('/feed.xml')}>{m.rss_feed()}</a>
-<br />
-<br />
-<a href={resolve('/settings')}>{m.settings()}</a>
