3154745f250f1d6b65479fab6a7164f388a2ef85
Alexander Hildebrandt <alex@hilde.dev>
2025-12-05 15:16:34 +0100

    chore: posting frontend code cleanup

--- src/routes/posting/+page.svelte
+++ src/routes/posting/+page.svelte
@@ -10,12 +10,10 @@
 <a href={resolve('/posting/create')}>{m.create_posting()}</a>
 <br />
 <br />
-{#await data.blogs then blogs}
-  {#each blogs as blog (blog.id)}
-    <div>
-      {blog.title}
-      <a href={resolve('/read/[id]', { id: blog.id })}>{m.read()}</a>
-      <a href={resolve('/posting/[id]', { id: blog.id })}>{m.generic_edit()}</a>
-    </div>
-  {/each}
-{/await}
+{#each data.blogs as blog (blog.id)}
+  <div>
+    {blog.title}
+    <a href={resolve('/read/[id]', { id: blog.id })}>{m.read()}</a>
+    <a href={resolve('/(admin)/posting/[id]', { id: blog.id })}>{m.generic_edit()}</a>
+  </div>
+{/each}
