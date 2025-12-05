a116eaaea85cb4b3e5e9da2c15d1ac3d1c6084b6
Alexander Hildebrandt <alex@hilde.dev>
2025-12-05 17:05:55 +0100

    feat: if token exists, automatically add it to feed url

--- src/routes/feed.xml/+server.ts
+++ src/routes/feed.xml/+server.ts
@@ -3,6 +3,21 @@ import type { RequestHandler } from '@sveltejs/kit'
 import { findPostingsForUser } from '$lib/server/db'
 
 export const GET: RequestHandler = async ({ url, locals }) => {
+  // When the user navigates to `/feed.xml` while being logged in, we assume that they want to copy
+  // the URL to put into a reader. So we'll append their rss_token to the URL so it will give them
+  // the access they expect.
+  if (locals.user?.rssToken && url.searchParams.get('rss_token') !== locals.user.rssToken) {
+    url.searchParams.set('rss_token', locals.user.rssToken);
+    const appendedUrl = url.pathname + url.search;
+
+    return new Response(null, {
+      status: 302,
+      headers: {
+        'Location': appendedUrl
+      }
+    });
+  }
+
   const posts = await findPostingsForUser(locals.user?.id)
 
   const BASE_URL = url.origin
@@ -47,6 +62,7 @@ export const GET: RequestHandler = async ({ url, locals }) => {
 
   const xml_string = xml(rss, { declaration: true })
 
+  // todo add query param to path, if its not already there
   return new Response(xml_string, {
     headers: {
       'Content-Type': 'text/xml; charset=utf-8'
