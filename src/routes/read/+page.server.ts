7f67b9f11b5212eddd6ee7796ee823d335ab2db1
Alexander Hildebrandt <alex@hilde.dev>
2025-12-05 14:38:25 +0100

    chore: remove unused file

--- src/routes/read/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import type { PageServerLoad } from './$types'
-import { findAllPostings } from '$lib/server/db'
-
-export const load: PageServerLoad = async () => {
-  return { blogs: findAllPostings() }
-}
