225fa02bdb089b8f0ceebc621b97a4fbe0e39f69
Alexander Hildebrandt <alex@hilde.dev>
2025-12-10 22:30:16 +0100

    fix: unable to access /cdn/new

--- src/routes/cdn/[posting_id]/[filename]/+server.ts
+++ src/routes/cdn/[posting_id]/[filename]/+server.ts
@@ -1,18 +1,24 @@
 import { fetchImage } from '$lib/server/filesystem.ts'
 import { fileTypeFromBuffer } from 'file-type'
-import { findPosting } from '$lib/server/db'
+import { findAdmin, findPosting } from '$lib/server/db'
 import { Visibility } from '$lib/server/db/schema.ts'
 import { error } from '@sveltejs/kit'
 
 export async function GET({ params, locals }) {
-  const posting = await findPosting(params.posting_id)
-  if (!posting) {
-    return error(404, 'No posting posting found.')
-  }
+  if (params.posting_id === 'new') {
+    if (!locals.user || !await findAdmin(locals.user.id)) {
+      throw error(404, 'Only admins can access the "new" folder.')
+    }
+  } else {
+    const posting = await findPosting(params.posting_id)
+    if (!posting) {
+      throw error(404, 'No posting found.')
+    }
 
-  if (posting.visibility !== Visibility.Public && !locals.user) {
-    // Technically a 403, but we pretend it's a 404 instead to not leak information.
-    return error(404, 'Post not found')
+    if (posting.visibility !== Visibility.Public && !locals.user) {
+      // Technically a 403, but we pretend it's a 404 instead to not leak information.
+      throw error(404, 'Post not found')
+    }
   }
 
   const fileContent = await fetchImage(params.posting_id, params.filename)
